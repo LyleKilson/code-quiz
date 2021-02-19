4369d5136b41aa4c73ad56c7659acefbe6a7a290
Lyle Kilson <lylekilsongithub@gmail.com>
2021-02-19 16:52:39 -0700

    add revamp

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
 // Variable for start button
-var startQuizBtn = document.querySelector("button");
+var startBtn = document.querySelector("button");
 
-//Variables for time left and high score
-
-var timer;
+// Time and score variables
+var score = 0;
 var timeLeft = 0;
+var currentQuestion = 0;
+var timer;
 
-//Arrray of questions and answers
-var questions = [
+//Arrray of questionsArr and answers
+var questionsArr = [
   {
     question: "Inside which HTML element do we put the JavaScript?",
     choices: ["<script>", "<js>", "<javascript>", "<scripting"],
@@ -19,6 +20,7 @@ var questions = [
       "The <body> section",
       "The <head> section",
       "Both the <head> section and the <body section are correct.",
+      "The <footer> section",
     ],
     answer: "The <body> section",
   },
@@ -29,13 +31,14 @@ var questions = [
       '<script src="xxx.js">',
       '<script name="xxx.js">',
       '<script href="xxx.js">',
+      '<script get="xxx.js">',
     ],
     answer: '<script src="xxx.js">',
   },
   {
     question: "The external JavaScript file must contain the <script> tag.",
-    choices: ["True", "False"],
-    answer: "True",
+    choices: ["True", "False", "truthy", "falsey"],
+    answer: "true",
   },
   {
     question: 'How do you write "Hello World" in an alert box?',
@@ -50,26 +53,31 @@ var questions = [
 ];
 
 //Function to start the quiz
-function startTimer() {
-  timeLeft = 90;
-  document.getElementById("timeLeft").innerHTML = timeLeft;
-  timer = setInterval(function () {
+function startQuiz() {
+  var timeLeft = 90;
+  var timer = setInterval(function () {
+    document.getElementById("timer").innerHTML = timeLeft;
     timeLeft--;
-    document.getElementById("timeLeft").innerHTML = timeLeft;
-    if (timeLeft <= 0) {
+    if (timeLeft === 0) {
       clearInterval(timer);
+      document.getElementById("timer").innerHTML = "0";
       endQuiz();
     }
-  }, 1000);
-
+  }, 100);
   startQuestions();
 }
 
-function startQuestions() {
-    for (var i = 0; i < questions.length; i++) {
-        console.log(questions);
-    }
-}
+var startQuestions = function () {
+  var displayQuestion = document.getElementById("questionBody");
+  displayQuestion.innerHTML = questionsArr[currentQuestion].question;
+  var dispalyChoices = document.getElementById("button");
+  dispalyChoices.textContent= questionsArr[currentQuestion].choices;
+};
 
 //Button click to call startQuiz()
-startQuizBtn.addEventListener("click", startTimer);
+startBtn.addEventListener("click", startQuiz);
+
+//if ( currentQuestion < questionsArr.length) {
+//currentQuestion++;
+
+//document.getElementById("quizBody").innerHTML = "<span>" + questionsArr[0].question + "</span>";
